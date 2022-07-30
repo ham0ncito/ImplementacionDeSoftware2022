859778a9627de448edc27efa7e7947fbfb1db38b
ham0ncito <lil_ham0n@hotmail.com>
2022-07-29 19:45:47 -0600

    sd

--- GerizimZZ/Clases/Cl_Repartidores.cs
+++ GerizimZZ/Clases/Cl_Repartidores.cs
@@ -1,14 +1,22 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+using GerizimZZ.Clases;
 
-namespace GerizimZZ.Clases
+namespace GerizimZZ
 {
     internal class Cl_Repartidores: Cl_conexiones
     {
         
+
+        //Aquí se realiza la consulta para llenar el DataGridView utilizando datos de dos tablas distintas
         public void infogrid(DataGridView grid)
         {
-            SqlCommand cm = new SqlCommand("select * from Repartidores ;", conex);
+            SqlCommand cm = new SqlCommand("select repartidores.ID_repartidores, repartidores.nombreRepartidor,repartidores.telefonoRepartidor,repartidores.ID_sucursal,pedidos.direccionEntrega,Pedidos.ID_factura From repartidores INNER JOIN pedidos ON repartidores.ID_repartidores = pedidos.ID_repartidores ;", conex);
             SqlDataAdapter da = new SqlDataAdapter(cm);
             DataTable dt = new DataTable();
 
@@ -18,3 +26,6 @@ namespace GerizimZZ.Clases
         }
     }
 }
+
+
+
